261909159a780ba939d5276e8c6b8c4606c014bc
Michal Wanat <michalwanat@gmail.com>
2015-03-26 19:33:52 +0100

    packages versions

--- package.js
+++ package.js
@@ -2,21 +2,22 @@
 
 Package.describe({
     name: 'vazco:universe-core-plugin',
-    summary: 'Universe CMS Plugin package'
+    summary: 'Universe CMS Plugin package -- DEPRECATED',
+    version: '0.0.3'
 });
 
 Package.on_use(function (api) {
     api.use([
-        'underscore',
-        'vazco:universe-core',
-        'aldeed:collection2',
-        'iron:router'
+        'underscore@1.0.1',
+        'vazco:universe-core@1.5.2',
+        'aldeed:collection2@2.2.0',
+        'iron:router@1.0.0'
     ]);
 
-    api.use(['templating', 'ui'], 'client');
+    api.use(['templating@1.0.9', 'ui@1.0.4'], 'client');
 
     api.imply([
-        'vazco:universe-core'
+        'vazco:universe-core@1.5.2',
     ]);
 
     api.add_files([
